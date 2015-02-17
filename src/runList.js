dcef0297f1e6ab9781d9ca4fef5cc8b612d306e2
Jim McAndrew <jim@loc8.us>
2015-02-17 12:39:26 -0700

    Fix obj

--- src/runList.js
+++ src/runList.js
@@ -1,6 +1,19 @@
 var Bluebird = require('bluebird');
-var fandlebarsObj = require('fandlebars').obj;
-
+var applyParams = function(params, tasks, results) {
+  return params.map(function(param) {
+    var returnValue;
+    if (typeof param === 'string' && param.match(/^\{\{.+?\}\}/g)) {
+      tasks.map(function(task, i) {
+        if (results.length >= i, task.name === param.replace(/^\{\{|\}\}/g, '')) {
+          returnValue = results[i];
+        }
+      });
+    } else {
+      returnValue = param;
+    }
+    return returnValue;
+  });
+};
 module.exports = function(list, taskName) {
   var messages = [];
   return new Bluebird(function(listResolve, listReject) {
@@ -8,13 +21,13 @@ module.exports = function(list, taskName) {
     var exec = function(sublist, msgList, callback) {
       var nextList = [];
       var params = Array.isArray(sublist[0].params) ? sublist[0].params : [sublist[0].params];
-      params = fandlebarsObj(params, {'msgList': msgList});
+      params = applyParams(params, list, msgList);
       sublist[0].task.apply(
           sublist[0].context,
           params
         )
         .then(function(msg) {
-          messages.push(msg);
+          messages[messages.push(msg)].name = sublist[0].name;
           nextList = sublist.slice(1);
           if (nextList.length > 0) {
             exec(nextList, messages, callback);
