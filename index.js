ededad0e7c77a3e3118e17e6a6f705e12c6e0a39
Jim McAndrew <jim@loc8.us>
2015-04-08 12:12:06 -0600

    Add mockingbird to datawrap: Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var returnValue = require('./src/datawrap');
+returnValue.Bluebird = require('Bluebird');
 returnValue.fandlebars = require('fandlebars');
+returnValue.mockingbird= require('./src/mockingbird');
 returnValue.runList = require('./src/runList');
 module.exports = returnValue;
