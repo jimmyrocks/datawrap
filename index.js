693b5db549cebd4b8b60378d7bba4d5f5fff5ca0
Jim McAndrew <jim@loc8.us>
2015-04-09 14:26:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var returnValue = require('./src/datawrap');
-returnValue.Bluebird = require('Bluebird');
+returnValue.Bluebird = require('bluebird');
 returnValue.fandlebars = require('fandlebars');
 returnValue.mockingbird= require('./src/mockingbird');
 returnValue.runList = require('./src/runList');
