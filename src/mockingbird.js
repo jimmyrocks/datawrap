557930db7fc642436d2439867fa2dffcc3f274be
Jim McAndrew <jim@loc8.us>
2015-04-09 14:24:52 -0600

    Create mockingbird.js

---
+++ src/mockingbird.js
@@ -0,0 +1,13 @@
+Bluebird = require('bluebird');
+
+module.exports = function(callback) {
+  return (callback && typeof callback === 'function') ? function(f) {
+    f(function(res) {
+        callback(null, res);
+      },
+      function(err) {
+        callback(err);
+      }
+    );
+  } : Bluebird;
+};
