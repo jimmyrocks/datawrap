f581da84c6fcd4b67b143b2076528f7f8fe13c42
Jim McAndrew <jim@loc8.us>
2016-01-15 15:43:47 -0700

    Support option for params, but still a work in progress

--- src/databases/sqlite.js
+++ src/databases/sqlite.js
@@ -1,20 +1,46 @@
 var Bluebird = require('bluebird');
+Bluebird.longStackTraces();
 var sqlite = require('sqlite3');
 var runList = require('../runList');
+var dbOpen = false;
+var databases = {};
+var connection = function (name, connection) {
+  var connectionDb = (dbOpen && name && databases[name]) ? databases[name] : new sqlite.Database(connection);
+  databases[name] = connectionDb;
+  dbOpen = true;
+  return {
+    all: function(query, callback) {
+      return connectionDb.all(query, callback);
+    },
+    close: function(force) {
+      if (!name || force) {
+        connectionDb.close();
+      } else {
+        return;
+      }
+    }
+  };
+};
 
 module.exports = function (config) {
   return {
     runQueryList: function (sql, params) {
       return new Bluebird(function (resolve, reject) {
+        var db = connection(config.name, config.connection || ':memory:');
+        if (sql[0] === 'close') {
+          db.close(true);
+          resolve(true);
+          return;
+        };
         if (!Array.isArray(sql)) {
           sql = [sql];
         }
 
-        var runQuery = function (query) {
+        var runQuery = function (query, params) {
           return new Bluebird(function (queryResolve, queryReject) {
+            // params = params || {};
             db.all(query, function (err, rows) {
               if (err) {
-                console.log('eRRor', err);
                 queryReject(err);
               } else {
                 queryResolve(rows);
@@ -28,12 +54,10 @@ module.exports = function (config) {
           taskList.push({
             'name': 'Query: ' + query,
             'task': runQuery,
-            'params': query
+            'params': [query, params]
           });
         });
 
-        console.log('list', taskList);
-        var db = new sqlite.Database(config.connection || ':memory:');
         runList(taskList, 'sqlite map')
           .then(function (r) {
             db.close();
