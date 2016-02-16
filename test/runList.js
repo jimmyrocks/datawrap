ad7ebf49799be0a4b6cb278c9c381de4e39755b2
Jim McAndrew <jim@loc8.us>
2016-02-16 13:36:30 -0700

    Clean up tests

--- test/runList.js
+++ test/runList.js
@@ -1,10 +1,10 @@
 var runList = require('../src/runList');
 var Bluebird = require('bluebird');
 
-var bbTest = function(time, returnValue, throwError) {
-  return new Bluebird(function(fulfill, reject) {
-    setTimeout(function() {
-      //do what you need here
+var bbTest = function (time, returnValue, throwError) {
+  return new Bluebird(function (fulfill, reject) {
+    setTimeout(function () {
+      // do what you need here
       if (throwError) {
         reject(new Error(returnValue));
       } else {
@@ -28,7 +28,7 @@ var testList = [{
   'params': [1000, 'test1000']
 }, {
   'name': 'Test 22',
-  'task': function(a, b, c) {
+  'task': function (a, b, c) {
     return a + b + c;
   },
   'params': [4, 5, 6]
@@ -38,7 +38,7 @@ var testList = [{
   'params': [1500, 'test1500']
 }, {
   'name': 'Test 22',
-  'task': function(a, b, c) {
+  'task': function (a, b, c) {
     return a * b * c;
   },
   'params': [4, 5, 6]
@@ -48,10 +48,9 @@ var testList = [{
   'params': [1230, 'test1230', false]
 }];
 
-
-runList(testList).then(function(x) {
+runList(testList).then(function (x) {
   console.log('resolved with', x);
-}).catch(function(x) {
+}).catch(function (x) {
   console.log('errored with', x);
   throw Array.isArray(x) ? x[x.length - 1] : x;
 });
